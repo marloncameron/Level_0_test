b7dd13b8a2581a9b2103c641879ed0e7288ef79a
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-04-19 17:10:01 +0200

    Update #task 06.py

--- #task 06.py
+++ #task 06.py
@@ -1,14 +1,14 @@
 #task 06
 
-def maximumvalue(a,b,c):
-    max = (a*b*c)
+def maximumvalue(list1):
+    max = list1[0]
+    for x in list1:
+        if x > max :
+             max = x
     return max
+    
 
-maxi1 = 3
-maxi2 = 5
-maxi3 = 8
 
-max = maximumvalue(maxi1,maxi2,maxi3)
+list1 = [1,22,3,2]
 
-print(max)
-
+print(maximumvalue(list1))
