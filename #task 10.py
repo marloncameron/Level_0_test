c8969d1fa799fc569a816a4486b1a9469f5d72ce
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-04-19 17:03:24 +0200

    Update #task 10.py

--- #task 10.py
+++ #task 10.py
@@ -1,12 +1,11 @@
 #task 10
 
-def compare (a ,b):
-    s1 = set(a)
-    s2 = set(b)
-    lst = list(s1 & s2)
-    print(lst)
+def comp():
+    a = 'house'
+    b = 'computers'
+    common_letters = set(a) & set(b)
+    print(list(common_letters))
 
-
-compare("house" , "computers")
+comp()
 
 
