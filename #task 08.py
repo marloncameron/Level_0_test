c01079bcd7367ad772049abcc48d00674123d339
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-04-22 12:07:00 +0200

    Update #task 08.py

--- #task 08.py
+++ #task 08.py
@@ -1,7 +1,22 @@
 #task 08
 
-def convertor(minutes):
-    print(minutes // 60 ,"hours" ,minutes % 60 ,"minutes")
+def conversion(minutes):
+    min_value = minutes % 60
+    hour_value = minutes // 60
 
-minutes = 133
-convertor(minutes)
+    #if hour is 0 minues is 0 or mre than 1 what should be printed
+
+    if hour_value == 0 and min_value == 0:
+        print(hour_value, "hours", min_value, "minutes")
+
+    elif hour_value == 1 and min_value == 1:
+        print(hour_value, "hour", min_value, "minute")
+
+    elif hour_value > 1 and min_value > 1:
+        print(hour_value, "hours", min_value, "minutes")
+
+    else:
+        print(hour_value, "hour", min_value, "minutes")
+
+min = 71
+conversion(min)
