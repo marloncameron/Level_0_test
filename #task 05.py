f6d66cb2b4208707ba6515bff827fe4272f9dc38
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-04-26 17:14:34 +0200

    Update #task 05.py

--- #task 05.py
+++ #task 05.py
@@ -2,16 +2,16 @@
 
 #the funtion below is herons formula 
 
-def areaoftriangle(a,b,c):
-    perimeter = a + b + c 
+def area_of_a_triangle(side_1, side_2, side_3):
+    perimeter = side_1 + side_2 + side_3
     s = perimeter / 2
-    area = (s*(s-a)*(s-b)*(s-c))**0.5
+    area = (s*(s-side_1)*(s-side_2)*(s-side_3))**0.5
     return area
 
-a1 = int(3)
-b1 = int(5)
-c1 = int(6)
+length_of_first_side = 3
+length_of_second_side = 5
+length_of_third_side = 6
 
-area = areaoftriangle(a1,b1,c1)
+area = area_of_a_triangle(length_of_first_side, length_of_second_side, length_of_third_side)
 
 print ("the area of the triangle is:" + str(area))
