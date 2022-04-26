850c9a8a51969b90dab5141e151b5607778b7aed
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-04-26 17:22:13 +0200

    Update #task 10.py

--- #task 10.py
+++ #task 10.py
@@ -1,12 +1,12 @@
 #task 10
 
 def compare_words(a ,b):
-    s1 = set(a)
-    s2 = set(b)
+    s1 = set(a.lower())
+    s2 = set(b.lower())
     lst = s1 & s2
-    print('Common letters:',lst)
+    print('Common letters:', lst)
 
 
-compare_words("house" , "computers")
+compare_words("housE", "computers")
 
 
