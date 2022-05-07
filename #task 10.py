8b01d10337ca3c1e39d4300e5a2651663b317094
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-07 13:29:39 +0200

    Update #task 10.py

--- #task 10.py
+++ #task 10.py
@@ -1,12 +1,15 @@
 #task 10
 
-def compare_words(a ,b):
-    s1 = set(a.lower())
-    s2 = set(b.lower())
-    lst = s1 & s2
-    print('Common letters:', *lst)
+def characters(x,y):
+    str1=x.lower()
+    str2=y.lower()
+    s=list(set(str1)&set(str2))
+    print("The common letters are:")
+    for i in s:
+        i = ', '.join(s)
+    print("Common characters: ",i)
 
 
-compare_words("housE", "computers")
+characters("housE", "computers");
 
 
