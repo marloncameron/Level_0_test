282399a80b96726848503313d6ff4c525cf2f037
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-07 13:04:15 +0200

    Update #task 06.py

--- #task 06.py
+++ #task 06.py
@@ -1,6 +1,6 @@
 #task 06
 
-def maximumvalue(list1):
+def maximum_value(list1):
     maximum = list1[0]
     for x in list1:
         if x > maximum :
@@ -11,4 +11,4 @@ def maximumvalue(list1):
 
 list1 = [1,22,3,2]
 
-print(maximumvalue(list1))
+print(maximum_value(list1))
