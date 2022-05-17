9635d05158ecd250d9ed5ee24fef7019cb58b5be
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-17 15:14:47 +0200

    Update #task_07.py

--- #task_07.py
+++ #task_07.py
@@ -1,21 +1,16 @@
-
-def Fahrenheit_to_celcuis(a):
-    cel = (a-32)*5/9
+def fahrenheit_to_celcuis(temp):
+    cel = (temp-32)*5/9
     return cel
 
 
-def celcuis_to_Fahrenheit(a):
-    fer = (a*1.8) + 32
+def celcuis_to_fahrenheit(temp):
+    fer = (temp*1.8) + 32
     return fer
 
-fh = 150
 
-celsuis =Fahrenheit_to_celcuis(fh)
-Fahrenheit =celcuis_to_Fahrenheit(fh)
+print(f"the temp is: {fahrenheit_to_celcuis(100)} Degrees ")
 
-print("the temp is:" + str(celsuis))
-
-print("the temp is:" + str(Fahrenheit))
+print(f"the temp is: {celcuis_to_fahrenheit(100)} Fahrenheit")
 
 
 
