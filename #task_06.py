61483185ff8aa9a7fe81b5fa31bb212423bc2922
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-17 15:05:05 +0200

    Update #task_06.py

--- #task_06.py
+++ #task_06.py
@@ -1,12 +1,8 @@
-def maximum_value(list1):
-    maximum = list1[0]
-    for x in list1:
-        if x > maximum :
-             maximum = x
-    return maximum
-    
+def maximumvalue(*list1):
+    maximum_num = list1[0]
+    for num in list1:
+        if num > maximum_num :
+             maximum_num = num
+    return maximum_num
 
-
-list1 = [1,22,3,2]
-
-print(maximum_value(list1))
+print(maximumvalue(1, 22 ,3,  2))
