7c3eace1acc0ed7277cea32dec7daa82303435a0
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-17 12:21:48 +0200

    Update #task_05.py

--- #task_05.py
+++ #task_05.py
@@ -4,10 +4,4 @@ def area_of_a_triangle(side_1, side_2, side_3):
     area = (semi_perimeter*(semi_perimeter-side_1)*(semi_perimeter-side_2)*(semi_perimeter-side_3))**0.5
     return area
 
-length_of_first_side = 3
-length_of_second_side = 5
-length_of_third_side = 6
-
-area = area_of_a_triangle(length_of_first_side, length_of_second_side, length_of_third_side)
-
-print ("the area of the triangle is:" + str(area))
+print(f" Area of triangle {area_of_a_triangle(3,5,6)}")
