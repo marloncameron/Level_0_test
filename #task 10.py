8bead85494f0057514e1d052e6e67869103f5868
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-17 09:49:21 +0200

    Update #task 10.py

--- #task 10.py
+++ #task 10.py
@@ -3,12 +3,10 @@ def characters(x,y):
     str1=x.lower()
     str2=y.lower()
     s=list(set(str1)&set(str2))
-    print("The common letters are:")
     for i in s:
         i = ', '.join(s)
     print("Common characters: ",i)
 
 
 characters("housE", "computers");
-
 
