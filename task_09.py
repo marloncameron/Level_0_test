0dbcc960c76b0b62dc30fbf9c255267bf29b67b3
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-18 14:46:50 +0200

    Update task_09.py

--- task_09.py
+++ task_09.py
@@ -1,14 +1,14 @@
-def compare(vowel):
+def output_vowels(word):
+    vowels = "Vowels: "
+    temp_string = word.lower()
+    for i in "aeiou":
+        if(temp_string.count(i) > 0):
+            vowels += i+", "
+    vowels = vowels[:-2]
+    return vowels
 
-    for vowel in 'aeiou':
 
-        if vowel in sentence.lower():
+print(output_vowels("Umuzi"))
 
-            print(vowel, end = ' ')
-
-
-sentence = "UmUzi"
-
-compare(sentence)
 
 
