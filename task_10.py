36bbbc0233ee6351207e754649da594607df18df
marloncameron <68697926+marloncameron@users.noreply.github.com>
2022-05-20 14:55:02 +0200

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,12 +1,11 @@
 
-def characters(x,y):
-    str1=x.lower()
-    str2=y.lower()
-    s=list(set(str1)&set(str2))
-    for i in s:
-        i = ', '.join(s)
-    print("Common characters: ",i)
+def characters(common_letter1,common_letter2):
+    str1=common_letter1.lower()
+    str2=common_letter2.lower()
+    common_str = list(set(str1)&set(str2))
+    for letters in common_str:
+        letters = ' , '.join(common_str)
+    print("Common characters: ",letters)
 
 
 characters("housE", "computers")
-
